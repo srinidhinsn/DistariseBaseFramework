cd65440a26e6afcd83f4728ed22324321eecd74d
Srinidhi N <srinidhinsn@gmail.com>
2020-08-03 22:11:35 +0100

    Initial project

---
+++ src/main/java/com/distarise/base/controller/UINav.java
@@ -0,0 +1,29 @@
+package com.distarise.base.controller;
+
+/**
+ * @author NSN
+ * @since 04/08/2020
+ */
+public class UINav {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UINav() {
+    }
+}
