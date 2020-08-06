2affe32e9fc191578bd5617c68b7e82807721480
snagarajan <snagarajan@whiteclarkegroup.com>
2020-08-07 02:13:55 +0530

    Added DB connection

--- src/main/java/com/distarise/base/Application.java
+++ src/main/java/com/distarise/base/Application.java
@@ -1,12 +1,15 @@
 package com.distarise.base;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author NSN
  * @since 04/08/2020
  */
+
 @SpringBootApplication
 public class Application {
 
