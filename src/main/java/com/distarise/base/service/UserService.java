d5a13d4f4b2220c12d9cf2d4fe47ecd56a7124f8
snagarajan <snagarajan@whiteclarkegroup.com>
2020-08-07 02:19:57 +0530

    Repository usage

--- src/main/java/com/distarise/base/service/UserService.java
+++ src/main/java/com/distarise/base/service/UserService.java
@@ -1,10 +1,12 @@
 package com.distarise.base.service;
 
+import com.distarise.base.repository.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    //@Autowired
-    //UserRepository userRepository;
+    @Autowired
+    UserDetailsRepository userRepository;
 }
