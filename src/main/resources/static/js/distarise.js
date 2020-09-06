03039ef31fecae92f228dcdaf5063323426c5632
snagarajan <snagarajan@whiteclarkegroup.com>
2020-09-06 21:50:47 +0530

    CSS changes

---
+++ src/main/resources/static/js/distarise.js
@@ -0,0 +1,9 @@
+function onClickSubmitForm (id, action, redirectUrl){
+    setDefaults(id, action, redirectUrl);
+    document.getElementById(id).submit();
+}
+
+function setDefaults(id, action, redirectUrl){
+    document.getElementById('action').value= action;
+    document.getElementById(id).action = redirectUrl;
+}
